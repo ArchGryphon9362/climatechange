aa034bd0ee402c4fb049a36c2d46c22613d1d37d
Lex Nastin <lexnastin@gmail.com@>
2022-04-04 09:25:58 +0100

    Create fade in Broken on button

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ class Presentation(tk.Canvas):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         b = tk.Button(self, text="Click to fade away", command=self.quit)
+        b.attributes("-alpha", 0)
         b.pack()
         self.parent = parent
         self.parent.attributes('-fullscreen', True)
         self.configure(bg='black')
+        self.fade_in(b)
 
     def quit(self):
         self.fade_away()
@@ -20,6 +22,13 @@ class Presentation(tk.Canvas):
             self.after(10, self.fade_away)
         else:
             self.parent.destroy()
+    
+    def fade_in(self, wgt):
+        alpha = wgt.attributes("-alpha")
+        if alpha < 1:
+            alpha += .01
+            wgt.attributes("-alpha", alpha)
+            self.after(10, self.fade_in, wgt)
 
 if __name__ == "__main__":
     root = tk.Tk()
