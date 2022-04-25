a79e31192c1aab9b0a993e47eae302c69ff3366d
Lex Nastin <lexnastin@gmail.com>
2022-04-25 09:43:15 +0100

    Cleanup + Create Placeholder Classes
    Also created some placeholder classes, which I'll need to begin filling
    out as quickly as possible, and begin working on the presentation
    itself!

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import tkinter as tk
 class Presentation(tk.Canvas):
     def __init__(self, parent):
         tk.Canvas.__init__(self, parent)
-        # b = tk.Button(self, text="Click to fade away", command=self.quit)
+        self.config(width=400, height=300)
+        self.resizable(False, False)
         self.rect1 = self.create_rectangle(50, 50, 150, 150, fill='red')
         self.tag_bind(self.rect1, "<1>", self.quit)
-        # b.attributes("-alpha", 0)
-        # b.pack()
         self.parent = parent
-        self.parent.attributes('-fullscreen', True)
+        # self.parent.attributes('-fullscreen', True)
         self.configure(bg='black')
         # self.fade_in(b)
+        print()
 
-    def quit(self, event):
+    def quit(self, event=None):
         print(str(event))
         self.itemconfig(self.rect1, fill='blue')
         self.fade_away()
@@ -26,13 +26,6 @@ class Presentation(tk.Canvas):
             self.after(10, self.fade_away)
         else:
             self.parent.destroy()
-    
-    def fade_in(self, wgt):
-        alpha = wgt.attributes("-alpha")
-        if alpha < 1:
-            alpha += .01
-            wgt.attributes("-alpha", alpha)
-            self.after(10, self.fade_in, wgt)
 
 if __name__ == "__main__":
     root = tk.Tk()
