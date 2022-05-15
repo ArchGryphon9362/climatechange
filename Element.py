3c42bf3d2937eb7ea1c4eb9d5ce488b2b82a4089
Lex Nastin <lexnastin@gmail.com>
2022-05-15 23:36:20 +0100

    Flesh Out Element Class

--- Element.py
+++ Element.py
@@ -1,15 +1,40 @@
-class Element:
-    def __init__(self):
-        pass
+from Presentation import Presentation
 
-    def move(self):
-        pass
+class Element:
+    def __init__(self, canvas: Presentation, id, show=False):
+        self.canvas = canvas
+        self.id = id
+        self.canvas.addtag_withtag("scale", self.id)
+        self.canvas.scale_unscaled()
+        if not show:
+            self.hide()
+
+    def move(self, x, y):
+        self.canvas.moveto(self.id, x, y)
     
-    def move_easeinout(self):
+    def move_easeinout(self, x1, y1, x2, y2, time, fps=30, show=False, hide=False):
+        if show:
+            self.show()
+        if hide:
+            self.hide()
+        
+        length = time * fps
+
+        for i in range (length):
+            x = easeInOutQuad(i, x1, x2, length)
+            y = easeInOutQuad(i, y1, y2, length)
+            
+            this.move(x, y)
+            root.update_idletasks()
+            root.update()
+            time.sleep(1 / fps)
         pass
 
     def show(self):
-        pass
+        self.canvas.itemconfigure(self.id, state='normal')
 
     def hide(self):
-        pass
+        self.canvas.itemconfigure(self.id, state='hidden')
+
+    def unload(self):
+        self.canvas.delete(self.id)
--- Text.py
+++ Text.py
@@ -1,6 +1,6 @@
 from Element import Element
 
-class Image(Element):
+class Text(Element):
     def __init__(self, text, font=None, size=12):
         Element.__init__(self)
         print(f'UNCOMPLETE FEATURE: Created label "{text}"')
