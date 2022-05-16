47ac1b33489fa4e09ad0df2866dc90700f2047ea
Lex Nastin <lexnastin@gmail.com>
2022-05-16 16:25:22 +0100

    Conjoin Text And Button

--- Button.py
+++ Button.py
@@ -7,7 +7,22 @@ from Element import Element
 class Button(Element):
     def __init__(self, canvas: tk.Canvas, x, y, w, h, fill="black", outline=None, hide=True, image: Image=None, text: Text=None, onclick=None):
         self.canvas = canvas
-        tags = (random.randint(0, 1000))
         id = self.canvas.create_rectangle(x, y, x + w, y + h, fill=fill, outline=outline)
         self.canvas.tag_bind(id, "<1>", onclick)
+        self.text = text
+        if self.text:
+            self.canvas.tag_bind(text.id, "<1>", onclick)
+            self.canvas.tag_raise(text.id)
         Element.__init__(self, canvas, id, w=w, h=h, hide=hide)
+
+    def move(self, x, y):
+        if self.text:
+            x1 = x + self.w / 2 - self.text.w / 2
+            y1 = y + self.h / 2 - self.text.h / 2
+            self.text.move(x1, y1)
+        Element.move(self, x, y)
+
+    def show(self):
+        if self.text:
+            self.text.show()
+        Element.show(self)
--- Flow.py
+++ Flow.py
@@ -14,9 +14,8 @@ class Flow:
 
         self.canvas = canvas
         self.canvas.configure(bg='white')
-        mb = Button(canvas, 0, 0, 100, 100, fill="#ff00ff", outline="#00ff00", onclick=print)
+        text = Text(canvas, 300, 200, "Hello!", color="#ccccff")
+        mb = Button(canvas, 0, 0, 100, 100, fill="#ff00ff", outline="#00ff00", text=text, onclick=print)
         mb.move_easeinout_to((987, 489), show=True)
         mb.move_easeinout_to((456, 346))
         mb.move_easeinout_to(mb.get_vh_centre())
-        text = Text(canvas, 300, 200, "Hello!", color="#ccccff", hide=False)
-        text.move_easeinout_to(text.get_vh_centre())
