f0bca394c1c9300afe64811a182f9d6bfb935386
Lex Nastin <lexnastin@gmail.com>
2022-05-16 19:43:02 +0100

    Finish First Screen

--- Flow.py
+++ Flow.py
@@ -15,3 +15,7 @@ class Flow:
         climate_change_text = Text(canvas, 0, 0, "Climate Change Informer", size=-50, color="white", bold=True)
         climate_change_text = Rectangle(canvas, -200, 150, 600, 80, text=climate_change_text, transparency=0.6)
         climate_change_text.move_easeinout_to(climate_change_text.get_h_centre(), show=True)
+        start_button = Text(canvas, 0, 0, "Start!", size=-30, bold=True)
+        start_button = Rectangle(canvas, 0, 720, 125, 75, text=start_button, fill="white", transparency=0.6)
+        start_button.move(*start_button.get_h_centre())
+        start_button.move_easeinout_to(start_button.get_vh_centre(), show=True)
