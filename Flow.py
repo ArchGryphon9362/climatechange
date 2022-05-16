bc3c18ccd038aece15ba5252a854349fa0b5d0f2
Lex Nastin <lexnastin@gmail.com>
2022-05-16 18:35:06 +0100

    Image Support
    It's semi-broken as image disappears right after the animations end...

--- Flow.py
+++ Flow.py
@@ -16,6 +16,8 @@ class Flow:
         self.canvas.configure(bg='white')
         text = Text(canvas, 300, 200, "Click Me!", color="#ccccff", bold=True)
         mb = Button(canvas, 0, 0, 100, 100, fill="#ff00ff", outline="#00ff00", text=text, onclick=print)
+        image = Image(canvas, 0, 0, 100, 100, "images/profile.png", hide=False)
+        # image.move_easeinout_to((0, 300), show=True)
         mb.move_easeinout_to((987, 489), show=True)
         mb.move_easeinout_to((456, 346))
         mb.move_easeinout_to(mb.get_vh_centre())
--- Image.py
+++ Image.py
@@ -1,5 +1,18 @@
+import tkinter as tk
+from tkinter import NW
 from Element import Element
+from PIL import ImageTk, Image as PImage
 
 class Image(Element):
-    def __init__(self):
-        Element.__init__(self)
+    def __init__(self, canvas: tk.Canvas, x, y, w, h, image, hide=True):
+        self.canvas = canvas
+        self.pil_image = PImage.open(image)
+        self.pil_image = self.pil_image.resize(
+            (
+                int(w * (self.canvas.w / 1280)),
+                int(h * (self.canvas.h / 720))
+            )
+        )
+        self.tk_image = ImageTk.PhotoImage(self.pil_image)
+        self.id = self.canvas.create_image(x, y, image=self.tk_image, anchor=NW)
+        Element.__init__(self, canvas, w, h, id, hide=hide)
