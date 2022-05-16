5a8f4b146c55df05c088a54338723819e111324d
Lex Nastin <lexnastin@gmail.com>
2022-05-16 20:40:20 +0100

    Fix Multiline Centering

--- Text.py
+++ Text.py
@@ -13,7 +13,12 @@ class Text(Element):
             slant=("italic" if italic else "roman")
         )
         id = self.canvas.create_text(x, y, fill=color, text=text, font=tk_font)
-        w = tk_font.measure(text) / (self.canvas.w / 1280)
+        largest_w = 0
+        for line in text.split("\n"):
+            width = tk_font.measure(line)
+            if (width > largest_w):
+                largest_w = width
+        w = largest_w / (self.canvas.w / 1280)
         h = tk_font.metrics()
         h = (h['ascent'] + h['descent']) / (self.canvas.h / 720)
         Element.__init__(self, canvas, w, h, id, hide=hide)
